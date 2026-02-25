3a8c825df6071fb3db8d81ed7e256bc47eab4d9a
ReBarsotti-HZB <rebecca.barsotti@helmholtz-berlin.de>
2026-02-25 13:46:45 +0000

    Added Doxygen annotations to db_field_log.h

--- modules/database/src/ioc/db/db_field_log.h
+++ modules/database/src/ioc/db/db_field_log.h
@@ -16,6 +16,35 @@
  *          Ralph Lange <Ralph.Lange@bessy.de>
  */
 
+/** @file db_field_log.h
+ * @brief Simple native types logged by `db_post_events()`
+ *  
+ * This is done for reliable interprocess communication, for any type 
+ * which is not a string or an array currently. Other types get whatever 
+ * happens to be there when the lower priority task pending on the event 
+ * queue wakes up. Strings would slow down events for more reasonable size 
+ * values. DB fields of native type string will most likely change 
+ * infrequently.
+ *
+ * Strings can be added to the set of types for which updates will be queued 
+ * by defining the macro `DB_EVENT_LOG_STRINGS`. The code in `db_add_event()` 
+ * will adjust automatically, it just compares field sizes.
+ *
+ * 
+ * A `db_field_log` has one of two types:
+ *
+ *  1. `dbfl_type_ref` - Reference to value
+ *  Used for variable size (array) data types.  Meta-data
+ *  is stored in the field log, but value data is stored externally.
+ *  Only the `dbfl_ref` side of the data union is valid.
+ *
+ *  2. `dbfl_type_val` - Internal value
+ *  Used to store small scalar data.  Meta-data and value are present in 
+ *  this structure and no external references are used.
+ *  Only the `dbfl_val` side of the data union is valid.
+ */
+ 
+
 #ifndef INCLdb_field_logh
 #define INCLdb_field_logh
 
@@ -26,18 +55,6 @@
 extern "C" {
 #endif
 
-/*
- * Simple native types (anything which is not a string or an array for
- * now) logged by db_post_events() for reliable interprocess communication.
- * (for other types they get whatever happens to be there when the lower
- * priority task pending on the event queue wakes up). Strings would slow down
- * events for more reasonable size values. DB fields of native type string
- * will most likely change infrequently.
- *
- * Strings can be added to the set of types for which updates will be queued
- * by defining the macro DB_EVENT_LOG_STRINGS. The code in db_add_event()
- * will adjust automatically, it just compares field sizes.
- */
 union native_value {
     epicsInt8 dbf_char;
     epicsUInt8 dbf_uchar;
@@ -55,44 +72,33 @@ union native_value {
 #endif
 };
 
-/*
- *  structure to log the state of a data base field at the time
- *  an event is triggered.
+
+ struct db_field_log; 
+/*!<  Structure to log the state of a data base field at the
+ * time an event is triggered.
  */
-struct db_field_log;
+
 typedef void (dbfl_freeFunc)(struct db_field_log *pfl);
 
-/*
- * A db_field_log has one of two types:
- *
- * dbfl_type_ref - Reference to value
- *  Used for variable size (array) data types.  Meta-data
- *  is stored in the field log, but value data is stored externally.
- *  Only the dbfl_ref side of the data union is valid.
- *
- * dbfl_type_val - Internal value
- *  Used to store small scalar data.  Meta-data and value are
- *  present in this structure and no external references are used.
- *  Only the dbfl_val side of the data union is valid.
- */
 typedef enum dbfl_type {
     dbfl_type_val,
     dbfl_type_ref
 } dbfl_type;
 
-/* Context of db_field_log: event = subscription update, read = read reply */
 typedef enum dbfl_context {
-    dbfl_context_read,
-    dbfl_context_event
-} dbfl_context;
+    dbfl_context_read, /*!< read = read reply */
+    dbfl_context_event /*!< event = subscription update */
+} dbfl_context; /*!< Context of db_field_log */
+
 
 #define dbflTypeStr(t) (t==dbfl_type_val?"val":"ref")
 
 struct dbfl_val {
-    union native_value field; /* Field value */
+    union native_value field; /*!< Field value */
 };
 
-/* External data reference.
+/**
+ * External data reference.
  * If dtor is provided then it should be called when the referenced
  * data is no longer needed.  This is done automatically by
  * db_delete_field_log().  Any code which changes a dbfl_type_ref
@@ -102,36 +108,36 @@ struct dbfl_val {
  * data is still owned by a record. See the macro dbfl_has_copy below.
  */
 struct dbfl_ref {
-    void              *pvt;   /* Private pointer */
-    void              *field; /* Field value */
+    void              *pvt;   /*!< Private pointer */
+    void              *field; /*!< Field value */
 };
 
 /*
  * Note that field_size may be larger than MAX_STRING_SIZE.
  */
 typedef struct db_field_log {
-    unsigned int     type:1;  /* type (union) selector */
+    unsigned int     type:1;  /*!< type (union) selector */
     /* ctx is used for all types */
-    unsigned int      ctx:1;  /* context (operation type) */
+    unsigned int      ctx:1;  /*!< context (operation type) */
     /* only for dbfl_context_event */
-    unsigned char      mask;  /* DBE_* mask */
+    unsigned char      mask;  /*!< DBE_* mask */
     /* the following are used for value and reference types */
-    epicsTimeStamp     time;  /* Time stamp */
+    epicsTimeStamp     time;  /*!< Time stamp */
     epicsUTag          utag;
-    unsigned short     stat;  /* Alarm Status */
-    unsigned short     sevr;  /* Alarm Severity */
+    unsigned short     stat;  /*!< Alarm Status */
+    unsigned short     sevr;  /*!< Alarm Severity */
     char               amsg[40];
-    short        field_type;  /* DBF type of data */
-    short        field_size;  /* Size of a single element */
-    long        no_elements;  /* No of valid array elements */
-    dbfl_freeFunc     *dtor;  /* Callback to free filter-allocated resources */
+    short        field_type;  /*!< DBF type of data */
+    short        field_size;  /*!< Size of a single element */
+    long        no_elements;  /*!< No of valid array elements */
+    dbfl_freeFunc     *dtor;  /*!< Callback to free filter-allocated resources */
     union {
         struct dbfl_val v;
         struct dbfl_ref r;
     } u;
 } db_field_log;
 
-/*
+/**
  * Whether a db_field_log* owns the field data. If this is the case, then the
  * db_field_log is fully decoupled from the record: there is no need to lock
  * the record when accessing the data, nor to call any rset methods (like
